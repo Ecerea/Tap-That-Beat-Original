9078d91ca61f06e5d78aaffb8eff1fcfbfe46b77
Ecerea <Flightfire@gmail.com>
2018-08-17 12:50:50 -0600

    Initial Commit

---
+++ Tap That Beat/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Tap That Beat
+//
+//  Created by JHartl on 8/17/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
